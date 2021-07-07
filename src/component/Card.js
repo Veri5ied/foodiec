0bc510f7f1097f6b6de80981de1a781a88e99d43
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2021-07-07 18:54:38 +0100

    learn more btn

--- src/component/Card.js
+++ src/component/Card.js
@@ -21,33 +21,38 @@ function Card() {
   }
 
   return (
-    <div className="card__body">
-      {food.map((el) => {
-        return (
-          <div className="card__container" key={el.id}>
-            <div className="card__img">
-              <img src={el.strMealThumb} alt="food cover" />
-            </div>
-            <div className="card__title">
-              <h3>{el.title}</h3>
-              <h3>{el.price}</h3>
-            </div>
-            <div className="card__meal">
-              <p>{el.strMeal}</p>
-            </div>
-            <div className="card__desc">
-              <p>{truncate(el.description, 100)}</p>
-            </div>
-            <div className="card__rating">
-              <div className="ratings">{`${el.ratings} stars`}</div>
-              <div className="btn">
-                <button className="plus">+</button>
+    <>
+      <div className="card__body">
+        {food.map((el) => {
+          return (
+            <div className="card__container" key={el.id}>
+              <div className="card__img">
+                <img src={el.strMealThumb} alt="food cover" />
+              </div>
+              <div className="card__title">
+                <h3>{el.title}</h3>
+                <h3>{el.price}</h3>
+              </div>
+              <div className="card__meal">
+                <p>{el.strMeal}</p>
+              </div>
+              <div className="card__desc">
+                <p>{truncate(el.description, 100)}</p>
+              </div>
+              <div className="card__rating">
+                <div className="ratings">{`${el.ratings} stars`}</div>
+                <div className="btn">
+                  <button className="plus">+</button>
+                </div>
               </div>
             </div>
-          </div>
-        );
-      })}
-    </div>
+          );
+        })}
+      </div>
+      <div className="card__btn">
+        <button>Learn More</button>
+      </div>
+    </>
   );
 }
 
