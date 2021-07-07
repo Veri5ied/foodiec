e884f5ea15162ca25293053e71093355e66df908
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2021-07-07 12:45:40 +0100

    api consumption

--- src/component/Card.js
+++ src/component/Card.js
@@ -10,7 +10,7 @@ function Card() {
     axios
       .get("https://asm-dev-api.herokuapp.com/api/v1/food")
       .then((res) => {
-        setFood(res.data.data);
+        setFood(res.data.data.meals);
       })
       .catch((err) => {
         console.log(err);
@@ -18,27 +18,38 @@ function Card() {
   }, []);
 
   console.log(food);
+
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
+
   return (
-    <div className="card__container">
-      <div className="card__img">
-        <img src={Cover} alt="food cover" />
-      </div>
-      <div className="card__title">
-        <h3>Rose Muffen</h3>
-        <h3>12$</h3>
-      </div>
-      <div className="card__desc">
-        <p>
-          Choic of coke, fanta, sprite. Upgrade to large fries, and whopper
-          patty, add tender crisp patty and more...
-        </p>
-      </div>
-      <div className="card__rating">
-        <div className="ratings">5 stars</div>
-        <div className="btn">
-          <button className="plus">+</button>
-        </div>
-      </div>
+    <div className="card__body">
+      {food.map((el) => {
+        return (
+          <div className="card__container" key={el.id}>
+            <div className="card__img">
+              <img src={el.strMealThumb} alt="food cover" />
+            </div>
+            <div className="card__title">
+              <h3>{el.title}</h3>
+              <h3>{el.price}</h3>
+            </div>
+            <div className="card__meal">
+              <p>{el.strMeal}</p>
+            </div>
+            <div className="card__desc">
+              <p>{truncate(el.description, 100)}</p>
+            </div>
+            <div className="card__rating">
+              <div className="ratings">{el.ratings}</div>
+              <div className="btn">
+                <button className="plus">+</button>
+              </div>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
